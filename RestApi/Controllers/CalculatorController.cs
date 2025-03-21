5d812cf2609abe13a2c1fcffb19ac2988b2ed0f6
TsahiKamar <69112927+TsahiKamar@users.noreply.github.com>
2025-03-21 17:09:05 +0200

    Update CalculatorController.cs

--- RestApi/Controllers/CalculatorController.cs
+++ RestApi/Controllers/CalculatorController.cs
@@ -7,25 +7,24 @@ using RestApi.Models;
 using RestApi.BL;
 
 
-//authentication failed: Usage of ngrok requires a verified account and authtoken.
-//ERROR:  
-//ERROR:  Sign up for an account: https://dashboard.ngrok.com/signup
-//ERROR:  Install your authtoken: https://dashboard.ngrok.com/get-started/your-authtoken
+//NGROK 
+//Account : 
+//  user: tsahikamar@gmail.com
+//  Pass:tsahikamar
 
-//https://ngrok.com/downloads/mac-os
-
-//ngrok for online service - > internet 
-//run : ngrok http 5159 
-
-//Add Authtoken 
+//Token INSTALLATION
+//Install your authtoken: https://dashboard.ngrok.com/get-started/your-authtoken
+//Add Authtoken :
 //ngrok config add-authtoken 2uY4NCeEnzqpGsSLXXhQSp8y3wI_5fPzUAqqzg1TRxB4pSmc3
 //Authtoken saved to configuration file: /Users/tsahikamar/Library/Application Support/ngrok/ngrok.yml
 
-//Command
-//ngrok http http://localhost:5159
+//RestApi Service run command
+//dotnet run
 
-//Web Interface                 http://127.0.0.1:4040                                                                                      
+//NGROK Service run command - > Use for RestApi is Internet ONLINE
+//ngrok http 5159 
 
+//Current NGROK Internet url  
 //https://e45c-2a00-a040-192-6a3b-4ca5-b099-873-7be5.ngrok-free.app
 
 namespace RestApi.Controllers
